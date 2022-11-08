4b2cfe76af06743b33d7a85091a138488ba5299f
terrywan <terrywan@webank.com>
2022-11-08 17:23:53 +0800

    feat: 提交初始代码;有部分优化项未实现;

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # minaprogram-no-use-components
-minaprogram no use components
+一个用在原生小程序上的脚本
+
+### 主要功能
+根据微信提示的优化项:
+
+组件/页面的usingComponents会影响启动速度，应避免将多余的组件声明usingComponents中
+
+需要删除在usingComponents注册了，但没有真实在wxml真实使用的组件
+
+### 使用方式
+
+1. 更改脚本所在真实目录
+
+项目根目录
+```
+const rootDir = '../'
+```
+
+2. 使用node运行脚本
+
+```
+cross-env NODE_ENV=development BUILD_TYPE=develop  node script/deletNoUseComponents
+```
+
+其中
+
+```
+BUILD_TYPE === develop
+```
+会开启json文件的自动替换 否则只有提示功能
+
---
+++ delNoUseComponents.js
@@ -0,0 +1,112 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const env = process.env.BUILD_TYPE
+
+/** 项目根目录 */
+const rootDir = '../'
+
+// 小程序注册入口文件
+const entry = require(`${rootDir}/src/app.json`)
+
+/** 没用使用到tags文件计数 */
+let noUseTagsNumsPages = 0
+
+/** 文件映射 */
+const noUseTagPagesMap = {}
+
+/** 是否打log */
+const showLog = true
+
+/** 是否自动删除 */
+const isAutoDelete = env === 'develop'
+
+/** 小程序页面全路径 */
+const entries = []
+
+Object.keys(entry).forEach((i) => {
+  if (i === 'pages') {
+    entry[i] &&
+      entry[i].length > 0 &&
+      entry[i].forEach((j) => {
+        entries.push(`${j}`)
+      })
+  }
+  if (i === 'subPackages') {
+    entry[i].forEach((j) => {
+      if (j && j.pages && j.pages.length > 0) {
+        j.pages.forEach((k) => {
+          entries.push(`${j.root}/${k}`)
+        })
+      }
+    })
+  }
+})
+
+/**
+ * 查找当前页面内未使用的组件并记录
+ * @param {*} i 页面地址
+ * @param {*} json 注册组件的json
+ * @param {*} wxml 页面对应的xml
+ */
+const findNoUseComponents = (i, json, wxml) => {
+  const currentJSONComponents = []
+  const noUseTags = []
+  if (json.usingComponents) {
+    Object.keys(json.usingComponents).forEach((i) => {
+      currentJSONComponents.push(i)
+    })
+  } else {
+    console.log('there is no use components', i, json)
+  }
+
+  if (currentJSONComponents.length > 0) {
+    currentJSONComponents.forEach((i) => {
+      const tagsName = `<${i}`
+      if (wxml.indexOf(tagsName) < 0) {
+        noUseTags.push(i)
+      }
+    })
+  }
+  if (noUseTags.length > 0) {
+    noUseTagsNumsPages += 1
+    noUseTagPagesMap[i] = noUseTags
+  }
+}
+
+entries.forEach((i) => {
+  const jsonFile = resolve(__dirname, `${rootDir}src`, `${i}.json`)
+  const xmlFile = resolve(__dirname, `${rootDir}src`, `${i}.wxml`)
+  const jsonContent = fs.readFileSync(jsonFile, 'utf8')
+  const wxmlInfo = fs.readFileSync(xmlFile, 'utf8')
+  const jsonInfo = JSON.parse(jsonContent, 'utf8')
+  findNoUseComponents(i, jsonInfo, wxmlInfo)
+})
+
+console.log('\x1B[31m%s\x1B[0m', `总共有${noUseTagsNumsPages}个页面引用了tag却没有使用,考虑删除他们好吗?`)
+
+if (showLog) {
+  console.log('未使用引用组件映射 /n', noUseTagPagesMap)
+}
+
+if (isAutoDelete) {
+  Object.keys(noUseTagPagesMap).forEach((i) => {
+    const filePath = resolve(__dirname, `${rootDir}src`, `${i}.json`)
+    const fileContent = fs.readFileSync(filePath)
+    const json = JSON.parse(fileContent, 'utf8')
+    const { usingComponents } = json
+    const noUseTagArr = noUseTagPagesMap[i]
+    Object.keys(usingComponents).forEach((j) => {
+      if (noUseTagArr.includes(j)) {
+        delete usingComponents[j]
+      }
+    })
+    const jsonContent = JSON.stringify(json)
+    fs.writeFile(filePath, jsonContent, 'utf8', function (err) {
+      if (err) {
+        console.log('An error occured while writing JSON Object to File.')
+        return console.log(err)
+      }
+      console.log('JSON file has been saved.')
+    })
+  })
+}
